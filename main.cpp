5effc2e37e3ed59dc189669fed372b84b87a82e2
Robert Daniels <robdaniels2001@gmail.com>
2024-01-30 20:00:53 -0500

    Create GLFW window

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "3D Graphics", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Error creating GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwTerminate();
     return 0;
 }
