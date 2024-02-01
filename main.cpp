c77f553c0985df70d1fb983560a6accc175d412d
Robert Daniels <robdaniels2001@gmail.com>
2024-01-31 21:32:46 -0500

    Add vertex and fragment shader source code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+// Vertex shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+// Fragment shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 1.0f, 0.0f);\n"
+"}\n\0";
+
 int main()
 {
     // Initialize GLFW
