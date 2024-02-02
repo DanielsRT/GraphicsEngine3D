74ab3e88861730229800be4119161807d5454616
Robert Daniels <robdaniels2001@gmail.com>
2024-02-01 19:01:03 -0500

    Move vertex shader source code to resource folder

--- main.cpp
+++ main.cpp
@@ -2,13 +2,7 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-// Vertex shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+
 // Fragment shader source code
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
