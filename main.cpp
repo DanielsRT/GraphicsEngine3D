7a05ba9e33b00b12be81faab3f650f9d6bdede0e
Robert Daniels <robdaniels2001@gmail.com>
2024-02-01 20:23:01 -0500

    Change shape vertices

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@
 // vertices coordinates
 GLfloat vertices[] =
 {
-    -0.6f, -0.6f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-    0.6f, -0.6f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-    0.0f, 0.6f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
-    - 0.6f / 2, 0.6f * float(sqrt(3)) / 6, 0.0f, // Inner left
-    0.6f / 2, 0.6f * float(sqrt(3)) / 6, 0.0f, // Inner right
-    0.0f, -0.6f * float(sqrt(3)) / 3, 0.0f // Inner down
+    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+    0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+    0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+    -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+    0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+    0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
 };
 // Indices for vertex order
 GLuint indices[] =
