f4dce8e85fafa8b992830cd3215bb7efbae5816a
Robert Daniels <robdaniels2001@gmail.com>
2024-02-14 20:42:26 -0500

    Add textures and coordinates to the Model class

---
+++ Model.cpp
@@ -0,0 +1,198 @@
+#include"Model.h"
+
+Model::Model(const char* file)
+{
+	std::string text = get_file_contents(file);
+	JSON = json::parse(text);
+
+	Model::file = file;
+	data = getData();
+}
+
+std::vector<unsigned char> Model::getData()
+{
+	std::string bytesText;
+	std::string uri = JSON["buffers"][0]["uri"];
+
+	std::string fileStr = std::string(file);
+	std::string fileDirectory = fileStr.substr(0, fileStr.find_last_of('/') + 1);
+	bytesText = get_file_contents((fileDirectory + uri).c_str());
+
+	std::vector<unsigned char> data(bytesText.begin(), bytesText.end());
+	return data;
+}
+
+std::vector<float> Model::getFloats(json accessor)
+{
+	std::vector<float> floatVec;
+
+	unsigned int buffViewInd = accessor.value("bufferView", 1);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byteOffset", 0);
+	std::string type = accessor["type"];
+
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	unsigned int numPerVertex;
+	if (type == "SCALAR") numPerVertex = 1;
+	else if (type == "VEC2") numPerVertex = 2;
+	else if (type == "VEC3") numPerVertex = 3;
+	else if (type == "VEC4") numPerVertex = 4;
+	else throw std::invalid_argument("Type is invalid (not SCALAR, VEC2, VEC3, or VEC4)");
+
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	unsigned int lengthOfData = count * 4 * numPerVertex;
+	for (unsigned int i = beginningOfData; i < beginningOfData + lengthOfData; i) 
+	{
+		unsigned char bytes[] = { data[i++], data[i++], data[i++], data[i++] };
+		float value;
+		std::memcpy(&value, bytes, sizeof(float));
+		floatVec.push_back(value);
+	}
+
+	return floatVec;
+}
+
+std::vector<GLuint> Model::getIndices(json accessor)
+{
+	std::vector<GLuint> indices;
+
+	unsigned int buffViewInd = accessor.value("bufferView", 0);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byuteOffset", 0);
+	unsigned int componentType = accessor["componentType"];
+
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	if (componentType == 5125)
+	{
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 4; i)
+		{
+			unsigned char bytes[] = { data[i++], data[i++], data[i++], data[i++] };
+			unsigned int value;
+			std::memcpy(&value, bytes, sizeof(unsigned int));
+			indices.push_back((GLuint)value);
+		}
+	}
+	else if (componentType == 5123)
+	{
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 2; i)
+		{
+			unsigned char bytes[] = { data[i++], data[i++] };
+			unsigned short value;
+			std::memcpy(&value, bytes, sizeof(unsigned short));
+			indices.push_back((GLuint)value);
+		}
+	}
+	else if (componentType == 5122)
+	{
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 2; i)
+		{
+			unsigned char bytes[] = { data[i++], data[i++] };
+			short value;
+			std::memcpy(&value, bytes, sizeof(short));
+			indices.push_back((GLuint)value);
+		}
+	}
+
+	return indices;
+}
+
+std::vector<Texture> Model::getTextures()
+{
+	std::vector<Texture> textures;
+
+	std::string fileStr = std::string(file);
+	std::string fileDirectory = fileStr.substr(0, fileStr.find_last_of('/') + 1);
+
+	for (unsigned int i = 0; i < JSON["images"].size(); i++)
+	{
+		std::string texPath = JSON["images"][i]["uri"];
+
+		bool skip = false;
+		for (unsigned int j = 0; j < loadedTexName.size(); j++)
+		{
+			if (loadedTexName[j] == texPath)
+			{
+				textures.push_back(loadedTex[j]);
+				skip = true;
+				break;
+			}
+		}
+
+		if (!skip)
+		{
+			if (texPath.find("baseColor") != std::string::npos)
+			{
+				Texture diffuse = Texture((fileDirectory + texPath).c_str(), "diffuse", loadedTex.size());
+				textures.push_back(diffuse);
+				loadedTex.push_back(diffuse);
+				loadedTexName.push_back(texPath);
+			}
+			else if (texPath.find("metallicroughness") != std::string::npos)
+			{
+				Texture specular = Texture((fileDirectory + texPath).c_str(), "specular", loadedTex.size());
+				textures.push_back(specular);
+				loadedTex.push_back(specular);
+				loadedTexName.push_back(texPath);
+			}
+		}
+	}
+}
+
+std::vector<Vertex> Model::assembleVertices
+(
+	std::vector<glm::vec3> positions,
+	std::vector<glm::vec3> normals,
+	std::vector<glm::vec2> texUVs
+)
+{
+	std::vector<Vertex> vertices;
+	for (int i = 0; i < positions.size(); i++)
+	{
+		vertices.push_back
+		(
+			Vertex
+			{
+				positions[i],
+				normals[i],
+				glm::vec3(1.0f, 1.0f, 1.0f),
+				texUVs[i]
+			}
+		);
+	}
+	return vertices;
+}
+
+std::vector<glm::vec2> Model::groupFloatsVec2(std::vector<float> floatVec)
+{
+	std::vector<glm::vec2> vectors;
+	for (int i = 0; i < floatVec.size(); i)
+	{
+		vectors.push_back(glm::vec2(floatVec[i++], floatVec[i++]));
+	}
+	return vectors;
+}
+
+std::vector<glm::vec3> Model::groupFloatsVec3(std::vector<float> floatVec)
+{
+	std::vector<glm::vec3> vectors;
+	for (int i = 0; i < floatVec.size(); i)
+	{
+		vectors.push_back(glm::vec3(floatVec[i++], floatVec[i++], floatVec[i++]));
+	}
+	return vectors;
+}
+
+std::vector<glm::vec4> Model::groupFloatsVec4(std::vector<float> floatVec)
+{
+	std::vector<glm::vec4> vectors;
+	for (int i = 0; i < floatVec.size(); i)
+	{
+		vectors.push_back(glm::vec4(floatVec[i++], floatVec[i++], floatVec[i++], floatVec[i++]));
+	}
+	return vectors;
+}
--- Model.h
+++ Model.h
@@ -17,5 +17,25 @@ private:
 	const char* file;
 	std::vector<unsigned char> data;
 	json JSON;
+
+	std::vector<std::string> loadedTexName;
+	std::vector<Texture> loadedTex;
+
+	std::vector<unsigned char> getData();
+	std::vector<float> getFloats(json accessor);
+	std::vector<GLuint> getIndices(json accessor);
+	std::vector<Texture> getTextures();
+
+
+	std::vector<Vertex> assembleVertices
+	(
+		std::vector<glm::vec3> positions,
+		std::vector<glm::vec3> normals,
+		std::vector<glm::vec2> texUVs
+	);
+
+	std::vector<glm::vec2> groupFloatsVec2(std::vector<float> floatVec);
+	std::vector<glm::vec3> groupFloatsVec3(std::vector<float> floatVec);
+	std::vector<glm::vec4> groupFloatsVec4(std::vector<float> floatVec);
 };
 #endif
